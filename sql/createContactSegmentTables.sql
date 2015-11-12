f6648eca1b346260e688657bccfbe81707b5f08a
Erik Hommel <hommel@ee-atwork.nl>
2015-11-12 14:29:04 +0100

    work in progress after analysis discussions

--- sql/createContactSegmentTables.sql
+++ sql/createContactSegmentTables.sql
@@ -1,31 +1,24 @@
-CREATE TABLE IF NOT EXISTS civicrm_contact_segment (
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(75) NULL,
-  parent_id INT NULL,
-  is_active TINYINT NULL,
+CREATE TABLE IF NOT EXISTS civicrm_segment (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  name VARCHAR(128) NULL,
+  label VARCHAR(256) NULL,
+  parent_id INT UNSIGNED NULL,
   PRIMARY KEY (id),
   UNIQUE INDEX id_UNIQUE (id ASC))
   ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS civicrm_contact_segment_role (
-  id INT NOT NULL AUTO_INCREMENT,
-  segment_id INT DEFAULT NULL,
-  contact_id INT DEFAULT NULL,
-  role_type_id INT DEFAULT 1,
+CREATE TABLE IF NOT EXISTS civicrm_contact_segment (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  contact_id INT UNSIGNED DEFAULT NULL,
+  segment_id INT UNSIGNED DEFAULT NULL,
+  role_value VARCHAR(512) DEFAULT NULL,
   start_date DATE,
   end_date DATE,
-  is_active TINYINT NULL,
+  is_active TINYINT UNSIGNED NULL,
   PRIMARY KEY (id),
   UNIQUE INDEX id_UNIQUE (id ASC),
-  INDEX category (segment_id ASC),
-  INDEX contact (contact_id ASC),
-  INDEX segment_contact (segment_id ASC, contact_id ASC))
-  ENGINE = InnoDB;
-
-CREATE TABLE IF NOT EXISTS civicrm_contact_segment_setting (
-  id INT NOT NULL AUTO_INCREMENT,
-  label VARCHAR(80) NULL,
-  child_label VARCHAR(80) NULL,
-  PRIMARY KEY (id),
-  UNIQUE INDEX id_UNIQUE (id ASC))
+  KEY `fk_segment` (`segment_id`),
+  KEY `fk_segment_contact` (`contact_id`),
+  CONSTRAINT `fk_segment` FOREIGN KEY (`segment_id`) REFERENCES `civicrm_segment` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fk_segment_contact` FOREIGN KEY(`contact_id`) REFERENCES `civicrm_contact` (`id`) ON DELETE CASCADE)
   ENGINE = InnoDB;
--- sql/dropContactSegmentTables.sql
+++ sql/dropContactSegmentTables.sql
@@ -1,3 +1,2 @@
+DROP TABLE IF EXISTS civicrm_segment;
 DROP TABLE IF EXISTS civicrm_contact_segment;
-DROP TABLE IF EXISTS civicrm_contact_segment_role;
-DROP TABLE IF EXISTS civicrm_contact_segment_setting;
